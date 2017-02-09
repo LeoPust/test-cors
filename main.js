ec7bc041e16d2d4f6f7037c2d17591d2f8319504
Leonid Pustovit <host@autorambler.ru>
2017-02-10 00:21:07 +0300

    start

---
+++ main.js
@@ -0,0 +1,46 @@
+"use strict";
+let express         = require("express"),
+    app             = express(),
+    logger          = require("morgan"),
+    bodyParser      = require("body-parser"),
+    cookieParser    = require("cookie-parser"),
+    compression     = require("compression");
+
+app
+    .use(compression({ threshold: 0 }))
+    .use(logger('dev'))
+    .use(bodyParser.urlencoded({extended:false}))
+    .use(bodyParser.json())
+    .use(cookieParser())
+    .use("/js", express.static("js"))
+    .use("/css", express.static("css"))
+    .use("/templates", express.static("templates"))
+    .use("/node_modules", express.static("node_modules"))
+    .use("/fonts", express.static("fonts"));
+
+app.options("*",(req,res) => {
+    res
+        .header("Access-Control-Allow-Methods",setting['allow-method'].toString())
+        .header("Access-Control-Allow-Origin","*")
+        .header("Access-Control-Allow-Headers","Accept, Authorization, Content-Type")
+        .status(204)
+        .end();
+});
+
+app.post("/auth",(req,res) => {
+    res
+        .append('Set-Cookie', 'foo=bar; Path=/;')
+        .status(200)
+        .end();
+});
+
+app.get("/olo",(req,res) => {
+   res.status(400).json({Olo:"asdas"}).end();
+});
+
+
+
+
+app.listen(9200,() => {
+    console.log("SERVER::START");
+});
